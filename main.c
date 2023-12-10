e2398edc43c69abb3fa3abaa0ac9b79727492e68
kolterma <kolterma@hs-pforzheim.de>
2023-12-10 15:57:44 +0100

    inserted number 6 in the linked list

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ void main()
     insert_at_beginning(&head, 22);
     insert_at_beginning(&head, 3);
     insert_at_beginning(&head, 25);
+    insert_at_beginning(&head, 6);
     print_list(head);
     free_list(&head);
     return;
