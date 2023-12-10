4d251c8916c5941fa8b12ad89acb184f06e3d86f
kolterma <kolterma@hs-pforzheim.de>
2023-12-10 18:17:28 +0100

    added whitespaces in main for cleaner code

--- main.c
+++ main.c
@@ -7,11 +7,13 @@ void main()
     NODE* head = NULL;
     print_list(head);
     free_list(&head);
+    
     insert_at_beginning(&head, 8);
     insert_at_beginning(&head, 22);
     insert_at_beginning(&head, 3);
     insert_at_beginning(&head, 25);
     insert_at_beginning(&head, 6);
+
     print_list(head);
     free_list(&head);
     return;
