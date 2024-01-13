44c933e8327dadb5723bcebd16ee24c76b18bf74
kolterma <kolterma@hs-pforzheim.de>
2024-01-13 17:31:17 +0100

    added some printing in the main file

--- main.c
+++ main.c
@@ -15,9 +15,11 @@ void main()
     insert_at_beginning(&mylist, 5);
     insert_at_end(&mylist, 7);
 
-    //printf("%u\n", mylist.tail->next);
+    //printf("%u\n", mylist.tail->data);
     
     print_list(&mylist);
     free_list(&mylist);
+    print_list(&mylist);
+
     return;
 }
